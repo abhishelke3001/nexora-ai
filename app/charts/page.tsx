1947b8e11b01684ca1e17e424391b4e56e757b11
ABHIMANYU SHELKE <macbookpro@ABHIs-Macbook-AIR.local>
2026-09-15 19:42:10 +0530

    Fix chart signal effect import

--- app/charts/page.tsx
+++ app/charts/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BtcChart from "../components/BtcChart";
 import Sidebar from "../components/Sidebar";
 
